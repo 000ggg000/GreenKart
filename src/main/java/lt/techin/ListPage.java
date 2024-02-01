3696841e03f97ab7da9fc04aff62d13f8447ffe6
000ggg000 <goda.suminaite@stud.techin.lt>
2024-02-01 14:43:48 +0200

    tests added

--- src/main/java/lt/techin/ListPage.java
+++ src/main/java/lt/techin/ListPage.java
@@ -1,32 +1,74 @@
 package lt.techin;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListPage extends BasePage {
 
-@FindBy(css = "select#page-menu")
+    @FindBy(id = "page-menu")
     WebElement sorting;
 
-@FindBy(css = "[value='20']")
-WebElement value20;
+    @FindBy(css = "[value='20']")
+    WebElement value20;
+
+    @FindBy(css = "th:nth-of-type(1)")
+    WebElement sortingalphabetically;
+
+    @FindBy(css = "tr td:first-child")
+    List<WebElement> elements;
+
+    @FindBy(id = "search-field")
+    WebElement searchBar;
+
 
     public ListPage(WebDriver driver) {
 
         super(driver);
     }
-    public void waiting(){
+
+    public void waiting() {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
 
-    public void sortItems() {
+    public void sortItemsNumber() {
         sorting.click();
         value20.click();
 
 
     }
+
+    public void clickSortingAlphabetically() {
+        sortingalphabetically.click();
+    }
+
+    public List<String> getItemsNames() {
+
+        //      List<WebElement> elements = driver.findElements(By.cssSelector("tbody > tr:nth-of-type(1)"));
+
+        return elements.stream().map(WebElement::getText).toList();
+
+//        ArrayList<String> names = new ArrayList<>();
+//        for (WebElement element : elements) {
+//            names.add(element.getText());
+//        }
+
+    }
+
+
+    public void inputSearchbar(String word) {
+        searchBar.sendKeys(word);
+    }
+
+    public List<String> getItemsSortedByWord(String word) {
+
+        return elements.stream().map(WebElement::getText).filter(w->w.contains(word)).toList();
+        }
 }
--- src/test/java/lt/techin/ListPageTest.java
+++ src/test/java/lt/techin/ListPageTest.java
@@ -2,13 +2,61 @@ package lt.techin;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ListPageTest extends BasePageTest {
 
 
-@Test
-    void filterItems(){
-    ListPage listPage = new ListPage(driver);
-    listPage.sortItems();
-}
+    @Test
+    void filterItems() {
+        ListPage listPage = new ListPage(driver);
+        listPage.sortItemsNumber();
+        Assertions.assertEquals("20", listPage.value20.getText());
+
+    }
+
+    @Test
+    void sortingItems() {
+        ListPage listPage = new ListPage(driver);
+        listPage.sortItemsNumber();
+        Assertions.assertEquals("20", listPage.value20.getText());
+        listPage.clickSortingAlphabetically();
+        System.out.println(listPage.getItemsNames());
+        List<String> elementsFromPage = listPage.getItemsNames();
+        List<String> elementsSorted = elementsFromPage.stream().sorted().toList();
+        System.out.println(elementsSorted);
+        Assertions.assertEquals(elementsSorted, elementsFromPage);
+
+    }
+
+    @Test
+    void filterSearchFunctionalityByContaining() {
+        ListPage listPage = new ListPage(driver);
+        listPage.sortItemsNumber();
+        Assertions.assertEquals("20", listPage.value20.getText());
+//        waiting();
+        listPage.inputSearchbar("dr");
+        List<String> listAfterFilter = listPage.elements.stream().map(w -> w.getText()).map(String::toLowerCase).toList();
+       //        List<String> searchFromPage = listPage.getItemsSortedByWord("dr");
+       System.out.println(listAfterFilter);
+        Assertions.assertTrue(listAfterFilter.stream().allMatch(w->w.contains("dr")));
+
+
+    }
+
+    @Test
+    void filterSearchFunctionalityByHavingAll() {
+        ListPage listPage = new ListPage(driver);
+        listPage.sortItemsNumber();
+        Assertions.assertEquals("20", listPage.value20.getText());
+        List<String> searchFromPage = listPage.elements.stream().map(WebElement::getText).map(String::toLowerCase).filter(text -> text.contains("dr")).toList();
+        listPage.inputSearchbar("dr");
+        List<String> listAfterFilterInPage = listPage.elements.stream().map(w -> w.getText()).map(String::toLowerCase).toList();
+        System.out.println("list" + searchFromPage);
+        Assertions.assertEquals(listAfterFilterInPage, searchFromPage);
+
+
+    }
 }
